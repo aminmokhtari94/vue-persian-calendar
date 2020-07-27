63980d4ffcf36c1170694c5bf16e558903af166c
Mohammad Amin Mokhtari <amin.mokhtari94@gmail.com>
2020-07-27 10:56:15 +0430

    Delete v-icon in main

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import feather from 'vue-icon'
-Vue.use(feather, 'v-icon')
-
 //Vue Jalali Moment
 import moment from 'vue-jalali-moment'
 Vue.use(moment)
